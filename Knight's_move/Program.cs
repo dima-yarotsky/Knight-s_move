9641cbf1ccb9437aeeee7fb145567971fcecf8f2
dima-yarotsky <yarotskydima@gmail.com>
2023-05-19 10:11:41 +0600

    Добавьте файлы проекта.

---
+++ Knight's_move/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] dp = new int[n, m];
+        dp[0, 0] = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (i - 1 >= 0 && j - 2 >= 0)
+                    dp[i, j] += dp[i - 1, j - 2];
+
+                if (i - 2 >= 0 && j - 1 >= 0)
+                    dp[i, j] += dp[i - 2, j - 1];
+            }
+        }
+
+        Console.WriteLine(dp[n - 1, m - 1]);
+    }
+}
