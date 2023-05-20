f5cc0b4f942a24539cb0e2037dc92570c2a73648
dima-yarotsky <yarotskydima@gmail.com>
2023-05-20 10:51:52 +0600

    changes

---
+++ Knight's_move/KnightPathsCalculator.cs
@@ -0,0 +1,51 @@
+﻿using Knight_s_move;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knight_s_move
+{
+    public class KnightPathsCalculator
+    {
+        private const int FinalPostion = 1;
+
+        private int n;
+        private int m;
+
+        private int paths;
+
+        public KnightPathsCalculator(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+        }
+
+        public int GetPaths()
+        {
+            var startPositionX = this.n;
+            var startPositionY = this.m;
+
+            this.CountRoutes(startPositionX, startPositionY, 0, true);
+            return this.paths;
+        }
+        private void CountRoutes(int x, int y, int movesCount, bool isFirst = false)
+        {
+            if (x < FinalPostion || y < FinalPostion)
+            {
+                return;
+            }
+
+            if (x == FinalPostion && y == FinalPostion && !isFirst)
+            {
+                this.paths += (int)Math.Pow(2, movesCount);
+                return;
+            }
+
+            movesCount++;
+            this.CountRoutes(x - 1, y - 2, movesCount);
+            this.CountRoutes(x - 2, y - 1, movesCount);
+        }
+    }
+}
--- Knight's_move/Program.cs
+++ Knight's_move/Program.cs
@@ -1,27 +1,33 @@
-﻿using System;
+﻿using Knight_s_move;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
-class Program
+class KnightPaths
 {
-    static void Main(string[] args)
+    internal static class Program
     {
-        int n = int.Parse(Console.ReadLine());
-        int m = int.Parse(Console.ReadLine());
-
-        int[,] dp = new int[n, m];
-        dp[0, 0] = 1;
-
-        for (int i = 0; i < n; i++)
+        private static void Main(string[] args)
         {
-            for (int j = 0; j < m; j++)
-            {
-                if (i - 1 >= 0 && j - 2 >= 0)
-                    dp[i, j] += dp[i - 1, j - 2];
-
-                if (i - 2 >= 0 && j - 1 >= 0)
-                    dp[i, j] += dp[i - 2, j - 1];
-            }
+            Input(out var m, out var n);
+            var calc = new KnightPathsCalculator(m, n);
+            var result = calc.GetPaths();
+            Console.WriteLine(result);
         }
 
-        Console.WriteLine(dp[n - 1, m - 1]);
+        private static void Input(out int m, out int n)
+        {
+            Console.WriteLine("Введите число M:");
+            while (!int.TryParse(Console.ReadLine(), out m) ||  m is > 15 or < 1)
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число M от 1 до 15:");
+            }
+
+            Console.WriteLine("Введите число N:");
+            while (!int.TryParse(Console.ReadLine(), out n) || n is > 15 or < 1)
+            {
+                Console.WriteLine("Ошибка ввода! Введите целое число N от 1 до 15:");
+            }
+        }
     }
 }
